5004b76b43863fc807431a4e7a4633dcf1de35a4
inasekin <nasekinid8591@yandex.ru>
2024-12-09 13:46:42 +0300

    feat: добавил линтер

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,10 @@
 package hexlet.code;
 
-import static hexlet.code.Cli.GetName;
+import static hexlet.code.Cli.getName;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
-        GetName();
+        getName();
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void GetName() {
+    public static void getName() {
         Scanner inCli = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String name = inCli.next();
